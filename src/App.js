b46a84aec9999a350de105b40a3dd18711112eba
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-20 17:05:39 -0500

    Create components To Do Machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ToDoCounter } from "./ToDoCounter";
+import { ToDoSearch } from "./ToDoSearch";
+import { ToDoList } from "./ToDoList";
+import { ToDoItem } from "./ToDoItem";
+import { CreateToDoButton } from "./CreateToDoButton";
+// import './App.css';
+
+const toDo = [
+  {text: 'Paso 1', completed: false},
+  {text: 'Paso 2', completed: false},
+  {text: 'Paso 3', completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToDoCounter />
+
+    <ToDoSearch />
+
+    <ToDoList>
+      {toDo.map(todo => (
+        <ToDoItem key={todo.text} text={todo.text} />
+      ))}
+    </ToDoList>
+
+    <CreateToDoButton />
+    </React.Fragment>
   );
 }
 
---
+++ src/ToDoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ToDoCounter() {
+  return (
+    <h2>Has completado 2 de 3 TO DO</h2>
+  )
+}
+
+export { ToDoCounter };
