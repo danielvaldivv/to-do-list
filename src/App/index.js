3a225e2d51004e1990ecb86387541cfc2dde23b1
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-23 07:24:14 -0500

    Data persistence with Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -2,15 +2,25 @@ import React from "react";
 import { AppUI } from "./AppUI";
 // import './App.css';
 
-const defaultToDo = [
-  {text: 'Paso 1', completed: true},
-  {text: 'Paso 2', completed: false},
-  {text: 'Paso 3', completed: false},
-  {text: 'Correr', completed: false}
-];
+// const defaultToDo = [
+//   {text: 'Paso 1', completed: true},
+//   {text: 'Paso 2', completed: false},
+//   {text: 'Paso 3', completed: false},
+//   {text: 'Correr', completed: false}
+// ];
 
 function App() {
-  const [toDos, setToDos] = React.useState(defaultToDo);
+  const localStorageToDos = localStorage.getItem('TODOS_V1');
+  let parsedToDos;
+
+  if(!localStorageToDos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedToDos = [];
+  } else {
+    parsedToDos = JSON.parse(localStorageToDos)
+  }
+
+  const [toDos, setToDos] = React.useState(parsedToDos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedToDos = toDos.filter(todo => !!todo.completed).length
@@ -29,18 +39,24 @@ function App() {
 
   }
 
+  const saveToDos = (newToDos) => {
+    const stringifiedToDos = JSON.stringify(newToDos);
+    localStorage.setItem('TODOS_V1', stringifiedToDos)
+    setToDos(newToDos)
+  };
+
   const completeToDo = (text) => {
     const toDoIndex = toDos.findIndex(todo => todo.text === text);
     const newToDos = [...toDos];
     newToDos[toDoIndex].completed = true;
-    setToDos(newToDos);
+    saveToDos(newToDos);
   };
 
   const deleteToDo = (text) => {
     const toDoIndex = toDos.findIndex(todo => todo.text === text);
     const newToDos = [...toDos];
     newToDos.splice(toDoIndex, 1);
-    setToDos(newToDos)
+    saveToDos(newToDos)
   }
 
 
