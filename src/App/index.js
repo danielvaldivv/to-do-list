437868f2c814ae52605ea3127c3f5b6cda05fdd0
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-10-05 12:39:56 -0500

    [Add] Composición y colocación del estado en React

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,73 @@
 import React from "react";
-import { ToDoProvider } from "../ToDoContext";
-import { AppUI } from "./AppUI";
+// import { ToDoProvider } from "../ToDoContext";
+// import { ToDoCounter } from "../ToDoCounter";
+// import { ToDoList } from "../ToDoList";
+// import { ToDoSearch } from "../ToDoSearch";
+// import { AppUI } from "./AppUI";
 
 function App() {
+  const [state, setState] = React.useState('Estado compartido');
 
-  return (
-  <ToDoProvider>
-    <AppUI />
-  </ToDoProvider>
+  return(
+    <>
+      <ToDoHeader>
+        <ToDoCounter />
+        <ToDoSearch />
+      </ToDoHeader>
+
+      <ToDoList>
+        <ToDoItem state={ state } />
+      </ToDoList>
+    </>
   );
-
 }
+
+function ToDoHeader({ children }) {
+  return(
+    <>
+      <header>
+        {children}
+      </header>
+    </>
+  );
+}
+
+function ToDoList( {children}) {
+  return(
+    <section className="ToDolist-Container">
+    {children}
+    </section>
+  );
+}
+
+function ToDoCounter ({ children }) {
+  return (
+    <p>
+      ToDoCounter
+    </p>
+  );
+}
+
+function ToDoSearch () {
+  return (
+    <p>ToDoSearch</p>
+  );
+}
+
+function ToDoItem ({ state }) {
+  return (
+    <p>ToDoItem: { state }</p>
+  );
+}
+
+// function App() {
+
+//   return (
+//   <ToDoProvider>
+//     <AppUI />
+//   </ToDoProvider>
+//   );
+
+// }
 
 export default App;
